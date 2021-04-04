84d4de011d57911a0895eaad191ba0a0a2eaf4cb
shinuqureshi <shireen.qureshi078@gmail.com>
2021-04-04 11:49:38 +0530

    simple add method and test case

---
+++ src/com/test/TestClass.java
@@ -0,0 +1,23 @@
+package com.test;
+import static org.junit.Assert.*;
+
+
+import com.company.Main;
+
+public class TestClass {
+
+     Main main = new Main() ;
+
+    // for creating test case we need to import junit
+
+    public static  void add_test(int firstNumber ,int secondNumber)
+    {
+        int output =0;
+        //assertEquals(output ,Main.add(firstNumber , secondNumber));
+        assertEquals(4 ,Main.add(2 , 2));
+
+
+    }
+
+
+}
