41cda131743e21ac76cf79e0f79f848c1ab13967
= <=>
2021-04-06 08:03:34 +0530

    new java code changes

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public  static int add(int firsNumber ,int secondNumber)
+    {    int output =0 ;
+        if (firsNumber==0 || secondNumber==0)
+             return 0 ;
+        else
+            output = firsNumber + secondNumber ;
+         return output ;
+
+    }
+
+}
